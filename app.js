78c1e16f697f1cdf2fecf52da9d5f67d39a049d0
Mikolaj Henzel <mikolajhenzel@gmail.com>
2021-02-22 11:21:00 +0100

    First commit. Express.js server and index.ejs file

---
+++ app.js
@@ -0,0 +1,15 @@
+const { response } = require('express');
+const express = require('express')
+const app = express()
+ 
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+ 
+app.use((request, response) => {
+  response.status(404).render('404');
+});
+
+app.listen(3000)
